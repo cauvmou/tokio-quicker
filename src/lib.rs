9be838c2e8eaa877de4354df8779502a2e3e03dc
cauvmou <cauvmou.8855@gmail.com>
2024-01-28 18:57:09 +0100

    Fixed up docs

--- src/lib.rs
+++ src/lib.rs
@@ -5,17 +5,17 @@
 //! #### [Client](https://github.com/cauvmou/tokio-quic/blob/main/examples/client.rs)
 //!
 //! First create a `QuicSocket`.
-//! ```rs
+//! ```rust,ignore
 //! let mut connection = QuicSocket::bind("127.0.0.1:0")
 //!         .await?
 //!         .connect(Some("localhost"), "127.0.0.1:4433")
 //!         .await?;
 //! ```
 //! Then you can start opening new `QuicStream`s or receive incoming ones from the server.
-//! ```rs
+//! ```rust,ignore
 //! let mut stream = connection.bidi(1).await?;
 //! ```
-//! ```rs
+//! ```rust,ignore
 //! let mut stream = connection.incoming().await?;
 //! ```
 //! These implement the tokio `AsyncRead` and `AsyncWrite` traits.
@@ -24,11 +24,11 @@
 //!
 //! Again create a `QuicListener`.
 //!
-//! ```rs
+//! ```rust,ignore
 //! let mut listener = QuicListener::bind("127.0.0.1:4433").await?;
 //! ```
 //! Then you can use a while loop to accept incoming connection and either handle them directly on the thread or move them to a new one.
-//! ```rs
+//! ```rust,ignore
 //! while let Ok(mut connection) = listener.accept().await {
 //!     tokio::spawn(async move {
 //!         let mut stream = connection.incoming().await?;
