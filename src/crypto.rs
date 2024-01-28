01ab37b854b982af72e02ea5de6c1618d69d0d8e
cauvmou <cauvmou.8855@gmail.com>
2024-01-29 00:31:42 +0100

    Added meta-data

--- src/crypto.rs
+++ src/crypto.rs
@@ -13,7 +13,11 @@ use rand::RngCore;
 ///      2. Generate Random bytes.
 ///      3. Encrypt the the `Vec<u8>` from step 1 with those random bytes and the servers encryption secret.
 ///      4. Append the random bytes at the end of the newly encrypted `Vec<u8>`.
-pub(crate) fn mint_token(dcid: &ConnectionId<'_>, src: &SocketAddr, token_secret: &[u8]) -> Vec<u8> {
+pub(crate) fn mint_token(
+    dcid: &ConnectionId<'_>,
+    src: &SocketAddr,
+    token_secret: &[u8],
+) -> Vec<u8> {
     let octets = ip_to_octets(&src.ip());
     let instant = chrono::Utc::now().timestamp();
     let dcid = dcid.to_vec();
@@ -34,7 +38,7 @@ pub(crate) fn validate_token<'a>(
     token: &Vec<u8>,
     src: &SocketAddr,
     token_secret: &[u8],
-    expiration_duration: Option<i64>
+    expiration_duration: Option<i64>,
 ) -> Option<ConnectionId<'a>> {
     let random = &token[token.len() - 8..].to_vec();
     let tag = &mut token.clone()[token.len() - 24..token.len() - 8].to_vec();
@@ -53,7 +57,9 @@ pub(crate) fn validate_token<'a>(
         chrono::Utc,
     );
     let duration = chrono::Utc::now().signed_duration_since(time);
-    if octets == decrypted[..octets.len()] && duration.num_seconds() <= expiration_duration.unwrap_or(i64::MAX) {
+    if octets == decrypted[..octets.len()]
+        && duration.num_seconds() <= expiration_duration.unwrap_or(i64::MAX)
+    {
         Some(ConnectionId::from_vec(
             decrypted[&octets.len() + 8..].to_vec(),
         ))
@@ -72,28 +78,46 @@ fn ip_to_octets(ip: &std::net::IpAddr) -> Vec<u8> {
 
 #[cfg(test)]
 mod test {
+    use crate::crypto::{mint_token, validate_token};
+    use quiche::ConnectionId;
     use std::net::SocketAddr;
     use std::str::FromStr;
-    use quiche::ConnectionId;
-    use crate::crypto::{mint_token, validate_token};
 
     #[test]
     fn validate_token_test() {
-        let connection_id = ConnectionId::from_ref(&[6, 114, 85, 25, 219, 159, 94, 178, 209, 240, 238, 52, 117, 222, 236, 117]);
-        let token: &[u8] = &[115, 35, 157, 90, 216, 123, 207, 22, 84, 203, 149, 220, 123, 81, 54, 156, 226, 47, 232, 79, 22, 177, 112, 222, 89, 251, 74, 199, 205, 192, 37, 164, 237, 24, 118, 220, 146, 175, 166, 95, 226, 187, 170, 187, 136, 44, 61, 186, 78, 4, 121, 231];
-        let secret: &[u8] = &[193, 225, 35, 100, 179, 123, 28, 109, 213, 167, 40, 242, 57, 91, 85, 30];
+        let connection_id = ConnectionId::from_ref(&[
+            6, 114, 85, 25, 219, 159, 94, 178, 209, 240, 238, 52, 117, 222, 236, 117,
+        ]);
+        let token: &[u8] = &[
+            115, 35, 157, 90, 216, 123, 207, 22, 84, 203, 149, 220, 123, 81, 54, 156, 226, 47, 232,
+            79, 22, 177, 112, 222, 89, 251, 74, 199, 205, 192, 37, 164, 237, 24, 118, 220, 146,
+            175, 166, 95, 226, 187, 170, 187, 136, 44, 61, 186, 78, 4, 121, 231,
+        ];
+        let secret: &[u8] = &[
+            193, 225, 35, 100, 179, 123, 28, 109, 213, 167, 40, 242, 57, 91, 85, 30,
+        ];
         let socket_addr = SocketAddr::from_str("127.0.0.1:42267").unwrap();
 
-        assert_eq!(validate_token(&token.to_vec(), &socket_addr, secret, None), Some(connection_id));
+        assert_eq!(
+            validate_token(&token.to_vec(), &socket_addr, secret, None),
+            Some(connection_id)
+        );
     }
 
     #[test]
     fn mint_token_test() {
-        let connection_id = ConnectionId::from_ref(&[6, 114, 85, 25, 219, 159, 94, 178, 209, 240, 238, 52, 117, 222, 236, 117]);
-        let secret: &[u8] = &[193, 225, 35, 100, 179, 123, 28, 109, 213, 167, 40, 242, 57, 91, 85, 30];
+        let connection_id = ConnectionId::from_ref(&[
+            6, 114, 85, 25, 219, 159, 94, 178, 209, 240, 238, 52, 117, 222, 236, 117,
+        ]);
+        let secret: &[u8] = &[
+            193, 225, 35, 100, 179, 123, 28, 109, 213, 167, 40, 242, 57, 91, 85, 30,
+        ];
         let socket_addr = SocketAddr::from_str("127.0.0.1:42267").unwrap();
 
         let token = mint_token(&connection_id, &socket_addr, secret);
-        assert_eq!(validate_token(&token, &socket_addr, secret, None), Some(connection_id))
+        assert_eq!(
+            validate_token(&token, &socket_addr, secret, None),
+            Some(connection_id)
+        )
     }
 }
